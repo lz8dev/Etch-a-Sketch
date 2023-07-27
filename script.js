21930c02ce880f2b1a12ebf5fc05dcd4d3a6e395
lz8dev <kv.ramosh@gmail.com>
2023-07-27 17:32:33 -0500

    Created first 16x16 grid fully fit into container and the on-click drawing functionallity

--- script.js
+++ script.js
@@ -5,4 +5,46 @@ function createGrid(){
         pixel.classList.add('pixel');
         grid.appendChild(pixel  )
     }
+};
+createGrid();
+
+let pixels = document.getElementsByClassName('pixel'),
+    isDrawing = false;
+
+for (let pixel of pixels){
+    pixel.addEventListener('mousedown', function(){
+        isDrawing = true;
+        pixel.style.backgroundColor = 'blue';
+    });
+    /*pixel.addEventListener('mouseenter', function(){
+        if(isDrawing){
+            pixel.style.backgroundColor = 'blue';
+        } else if(getComputedStyle(pixel).backgroundColor === 'rgb(255, 255, 255)'){
+            pixel.style.backgroundColor = 'rgb(218, 218, 218)'; //Must be set in rgb to allow using getComputedStyle in the next listener
+        }
+    });*/
+    pixel.addEventListener('mouseenter', function(){
+        if(!isDrawing && getComputedStyle(pixel).backgroundColor === 'rgb(255, 255, 255)'){
+            pixel.style.backgroundColor = 'rgb(218, 218, 218)'; //Must be set in rgb to allow using getComputedStyle in the next listener
+        }else if(isDrawing){
+            pixel.style.backgroundColor = 'blue';
+        } else {
+            return;
+        }
+    });
+    pixel.addEventListener('mouseleave', function(){
+        if(!isDrawing && getComputedStyle(pixel).backgroundColor === 'rgb(218, 218, 218)'){
+            pixel.style.backgroundColor = 'rgb(255, 255, 255)';
+        } 
+    });
+    document.addEventListener('mouseup', function() {
+        isDrawing = false;
+    });
+}
+
+
+function clearGrid(){
+    for (let pixel of pixels){
+        pixel.style.backgroundColor = 'white';
+    };
 }
