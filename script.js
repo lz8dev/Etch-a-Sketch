61cb1f0cf2ed2250b42233624a923edbef0c63c9
lz8dev <kv.ramosh@gmail.com>
2023-07-28 18:33:26 -0500

    First attempt making the interface, aslo set classic styles for pixels

--- script.js
+++ script.js
@@ -18,14 +18,14 @@ function startDraw(){
         pixel.addEventListener('mousedown', function(event){
             event.preventDefault(); //Prevent default behavior of dragging 
             isDrawing = true;
-            pixel.style.backgroundColor = 'blue';
+            pixel.style.backgroundColor = 'rgb(100, 100, 100)';
         });
         
         pixel.addEventListener('mouseenter', function(){
             if(!isDrawing && getComputedStyle(pixel).backgroundColor === 'rgb(255, 255, 255)'){
                 pixel.style.backgroundColor = 'rgb(218, 218, 218)'; //Must be set in rgb to allow using getComputedStyle in the next listener
             }else if(isDrawing){
-                pixel.style.backgroundColor = 'blue';
+                pixel.style.backgroundColor = 'rgb(100, 100, 100)';
             } else {
                 return;
             }
@@ -44,6 +44,30 @@ function startDraw(){
 function clearGrid(){
     let pixels = document.getElementsByClassName('pixel');
     for (let pixel of pixels){
-        pixel.style.backgroundColor = 'white';
+        pixel.style.backgroundColor = 'rgb(230, 230, 230)';
     };
 }
+
+let penPick = document.getElementById('penPick'),
+    penColor = document.getElementById('penColor');
+
+    penPick .addEventListener('input', function(event){
+        let pickedColor = event.target.value;
+        penColor.style.backgroundColor = pickedColor;
+});
+
+/*
+    <label for="colorPicker">Choose a color:</label>
+    <input type="color" id="colorPicker" name="colorPicker">
+    <div id="colorPreview" style="width: 100px; height: 100px; margin-top: 20px;"></div>
+
+    <script>
+        const colorPicker = document.getElementById('colorPicker');
+        const colorPreview = document.getElementById('colorPreview');
+
+        colorPicker.addEventListener('input', (event) => {
+            const selectedColor = event.target.value;
+            colorPreview.style.backgroundColor = selectedColor;
+        });
+    </script>
+    */
